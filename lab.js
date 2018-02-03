8249c0bc5aecb1ec7858b195b4a51222456779e0
hanifsameer <36103016+hanifsameer@users.noreply.github.com>
2018-02-03 18:50:38 +0500

    Add files via upload

---
+++ lab.js
@@ -0,0 +1,5 @@
+window.prompt("please enter your name.");
+var userName = window.prompt("please enter your name.");
+var greetingParagraph = document.getElementById("greeting");
+   
+greetingParagraph.innerHTML += userName;
